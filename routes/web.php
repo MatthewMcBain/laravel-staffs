cf8e5be49f703a17708984327e0217a023037e62
Matthew McBain <matthewmcbain3@gmail.com>
2024-04-18 10:07:56 +0100

    no delete error

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('chirps', ChirpController::class)
-       ->only(['index', 'store', 'edit', 'update']);
+       ->only(['index', 'store', 'edit', 'update', 'destroy']);
     Route::post(
        '/chirps/{chirp}/addToFavourites',
        [ChirpController::class, 'addToFavourites']
